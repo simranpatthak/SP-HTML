d4bc18df5004cb01727be36aa042e5b7c8b56b15
simranpatthak <143158379+simranpatthak@users.noreply.github.com>
2024-04-05 22:28:51 +0530

    Add files via upload

---
+++ checkprime/index (15).js
@@ -0,0 +1,14 @@
+function isPrime(n){
+  for(let i=2; i<n;i++){
+    if(n%i===0){
+      return false
+    }
+  }
+  return true
+}
+
+for(let i=2; i<100;i++){
+  if(isPrime(i))
+    console.log(i);
+}
+
